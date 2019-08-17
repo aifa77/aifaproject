ec19a4d6eba22b8d4e8ee24a7a8a3c69a07f0e10
aifa77 <aifa.nur.amalia@gmail.com>
2019-08-17 21:57:04 +0700

    fix role user default value

--- database/migrations/2019_08_17_141336_create_role_user.php
+++ database/migrations/2019_08_17_141336_create_role_user.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoleUserTable extends Migration
+class CreateRoleUser extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('role_id')->default('1')->change();
+            $table->integer('role_id')->default(1);
             $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
